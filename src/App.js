3be4714d9bf57484fc54d879b922a05858c90686
darcy <darcy.u83@gmail.com>
2021-11-22 20:44:48 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: ${(props) => props.bgColor};
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: skyblue;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father as="header">
+      {" "}
+      as="header" 를 적어줌으로 써 div tag -> header tag로 변경
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Box bgColor="tomato" />
+      <Circle bgColor="lightgrey" />
+      <hr></hr>
+      <Btn as="a" href="/" bgColor="tomato">
+        Login
+      </Btn>
+      <Btn bgColor="tomato">Login</Btn>
+      <hr></hr>
+      {`styled.input.attrs({required:true}) ==> setAttributes(required,true)`}
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
   );
 }
 
