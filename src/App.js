e9a1fcc346f2bece5e3a4c3c8616d33727187784
darcy <darcy.u83@gmail.com>
2021-11-24 18:40:37 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+`;
+
+const rotationAnime = keyframes`
+from{transform: rotate(0deg); border-radius : 0px}
+to{transform : rotate(360deg) ; border-radius : 50px}
+`;
+
+const rotationAnime1 = keyframes`
+0%{transform : rotate(0deg); border-radius : 0px}
+50%{transform : rotate(360deg); border-radius : 50px}
+100%{transform : rotate(0deg); border-radius : 0px}
 `;
 
 const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnime1} 3s linear infinite;
+
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 100px;
+    }
+  }
+`;
+
+const BoxCnts = styled.span`
+  font-size: 36px;
+`;
+
+const BoxWCnts = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${BoxCnts}:hover {
+    font-size: 98px;
+  }
 `;
 
 const Text = styled.span`
@@ -32,8 +72,7 @@ const Input = styled.input.attrs({ required: true })`
 function App() {
   return (
     <Father as="header">
-      {" "}
-      as="header" 를 적어줌으로 써 div tag -> header tag로 변경
+      {" as=header 를 적어줌으로 써 div tag -> header tag로 변경 "}
       <Box bgColor="teal">
         <Text>Hello</Text>
       </Box>
@@ -47,9 +86,12 @@ function App() {
       <hr></hr>
       {`styled.input.attrs({required:true}) ==> setAttributes(required,true)`}
       <Input />
-      <Input />
-      <Input />
-      <Input />
+      <hr></hr>
+      <div>
+        <BoxWCnts bgColor="yellow">
+          <BoxCnts>★</BoxCnts>
+        </BoxWCnts>
+      </div>
     </Father>
   );
 }
