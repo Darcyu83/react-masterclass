dc376d5f3cb930235e58ed283fa7a466116987f6
darcy <darcy.u83@gmail.com>
2021-11-24 19:26:16 +0900

    styled-components Theme

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
-  box-sizing: border-box;
-  display: inline-block;
-  width: 700px;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const anime = keyframes`
-from {
-  color : tomato
-}
-to{
-  color : teal
-}`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  margin: 0px auto;
-  background-color: ${(props) => props.bgColor};
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const H1GoHOme = styled.h1`
-  color: blue;
-`;
-
-const Btn = styled.button.attrs({ disabled: true })`
-  color: tomato;
-  animation: ${anime} 1s infinite;
-
-  margin: 0px auto;
-  ${H1GoHOme}:hover {
-    font-size: 98px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="tomato"></Box>
-      <Box bgColor="teal"></Box>
-      <Circle bgColor="cyan"></Circle>
-      <hr></hr>
-      <Btn>Login</Btn>
-      <Btn as="a">
-        <H1GoHOme>Go Home</H1GoHOme>
-      </Btn>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
